d561b1640911f77da3f19cf25c65018f5fa22c03
Suryab02 <bsprabhas2002@gmail.com>
2023-08-31 20:14:40 +0530

    app completed

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,15 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import fectPet from "./fectPet";
+import { useState } from "react";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
+import Carousel from "./Carousel";
 const Details = () => {
   const { id } = useParams();
 
   const results = useQuery(["details", id], fectPet);
-
+  const [showModal, setShowModal] = useState(false);
   if (results.isLoading) {
     return (
       <div className="loading-pane">
@@ -17,16 +21,38 @@ const Details = () => {
 
   return (
     <div className="details">
+      <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal} - {pet.breed} - {pet.city} , {pet.state}
         </h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>;
         <p>{pet.description}</p>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null // you have to remove this semi-colon, my auto-formatter adds it back if I delete it
+        }
       </div>
     </div>
   );
 };
 
-export default Details;
+function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
+
+export default DetailsErrorBoundary;
---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          Error go to Home PAGE.<Link to="/">Click here</Link>
+        </h2>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
